597d94a20c4eef2fd9bee49108f9dd70cb293b28
rayhantmt <rayhantmt@gmail.com>
2026-07-02 22:56:07 +0600

    initiated the project

---
+++ lib/auth/splashscreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+    
+            SizedBox(height: 20),
+            Text('Loading...'),
+          ],
+        ),
+      ),
+    );
+  }
+}
